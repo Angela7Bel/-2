fc2ff7add6478f97fc24371e1bfc4fac0e7062d1
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2022-12-18 00:28:23 +0700

    Add files via upload

---
+++ 1/obj/2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2
+Console.Clear();
+Console.Write("а =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b =");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c =");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
