b4503a02d9cb4a1796b4cd6d0f36e0d07274e1d5
Angela <angela.beljaeva@mail.ru>
2022-12-17 22:45:13 +0700

    01

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1
+Console.Clear();
+Console.Write("а =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b =");
+int b = Convert.ToInt32(Console.ReadLine());
+ if (a > b) 
+    Console.WriteLine("a = max b = min");
+ else
+ if (b > a)
+    Console.WriteLine("b = max a = min");
+
+
